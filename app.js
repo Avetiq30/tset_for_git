9160e626fd9746090cc7776f2ff646c5ee538679
AVETIK <avetik@MacBook-Pro-AVETIK.local>
2024-01-23 12:23:15 -0800

    main branch

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-
-console.log('hello  branch 1 commit 1');
-console.log('hello  branch 1 commit 2');
-console.log('hello  branch 2 commit 3');
-console.log('hello  branch main commit 4');
-console.log('move this commit');
-console.log('hello  commit 555555  commit');
-console.log('hello  commit 555 __ 2');
-console.log('hello  commit 555 __ 1');
-console.log('hello  commit 555 second');
-console.log('hello  commit 555 second');
-
-console.log('hello secondCCCC');
-
-console.log('halloooooooo');
+console.log('barnch main hello');
