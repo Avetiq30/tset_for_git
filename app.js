5e028c2dd749b21d7fd5a0fdf3f244942fe9b122
AVETIK <avetik@MacBook-Pro-AVETIK.local>
2024-01-23 11:52:43 -0800

    second

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ console.log('hello  branch 1 commit 2');
 console.log('hello  branch 2 commit 3');
 console.log('hello  branch main commit 4');
 console.log('hello  commit 555 __ 1');
+console.log('hello  commit 555 second');
+console.log('hello  commit 555 second');
 
