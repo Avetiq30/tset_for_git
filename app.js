3f119d15fd6b2a1db5df8b62a54be2657578cf45
AVETIK <avetik@MacBook-Pro-AVETIK.local>
2024-01-24 01:13:24 -0800

    create 4

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log('barnch main hello');
 console.log('barnch create1 hello');
 console.log('barnch create2 hello');
 console.log('barnch create3 hello');
+console.log('barnch create4 hello');
